6b2d1d2dad400ed919f5afd58f8ec7d42ea695dc
Naymur000 <nayeemur000@gmail.com>
2026-04-15 13:57:08 +0600

    u

--- conditional.py
+++ conditional.py
@@ -1,6 +1,17 @@
-r = 1
+# r = 1
 
-if r == 1:
-    print("I will go to university.")
+# if r == 1:
+#     print("I will go to university.")
+# else:
+#     print("I will not go to university")
+
+
+r = int(input("Enter value : "))
+
+if r > 0:
+    print(r, " is positive")
+elif r < 0:
+    print(r, " is negative")
 else:
-    print("I will not go to university")
+    print(r," is zero")
+    
