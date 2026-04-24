315a45858a57ec0894f93a9d540ff64d7c8cbea4
Naymur000 <nayeemur000@gmail.com>
2026-04-24 14:34:15 +0600

    args

---
+++ Module-2/argument.py
@@ -0,0 +1,7 @@
+def addition(*args):
+    print(args)
+    return sum(args)
+    
+r = addition(7,8,9,12,23,45)
+
+print(r)
--- Module-2/function.py
+++ Module-2/function.py
@@ -3,3 +3,4 @@ print("Hello")
 user_name = input("Enter your name: ")
 
 print(f"Hello {list({user_name*2})}")
+
