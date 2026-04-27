48c62590d69de42280f9a7b65fc7f24db8b23ceb
Naymur000 <nayeemur000@gmail.com>
2026-04-27 16:12:17 +0600

    line

--- Module-2/file-handling.py
+++ Module-2/file-handling.py
@@ -6,3 +6,15 @@ with open('name.txt', 'r') as f:
 with open("name.txt", "w") as f:
     f.write("This is Naymur before you\n")
     f.write("BSC in CSE\n")
+    
+with open("name.txt", "a") as f:
+    f.write("Living in Mymensingh\n")
+    f.write("I love to code\n")
+    
+
+line = ['\nI love python\n', 'I am new to python\n']    
+
+with open("name.txt", "a") as f:
+    f.writelines(line)
+
+print(content)
