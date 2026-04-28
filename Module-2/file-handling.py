8f55eee28dc2227b3e7427ba6ad62b13b2f753f6
Naymur000 <nayeemur000@gmail.com>
2026-04-28 17:00:21 +0600

    t

--- Module-2/file-handling.py
+++ Module-2/file-handling.py
@@ -1,20 +1,35 @@
+import os
+import pathlib
+
+
 with open('name.txt', 'r') as f:
     content = f.read()
     print(content)
     
 
-with open("name.txt", "w") as f:
-    f.write("This is Naymur before you\n")
-    f.write("BSC in CSE\n")
+# with open("name.txt", "w") as f:
+#     f.write("This is Naymur before you\n")
+#     f.write("BSC in CSE\n")
     
-with open("name.txt", "a") as f:
-    f.write("Living in Mymensingh\n")
-    f.write("I love to code\n")
+# with open("name.txt", "a") as f:
+#     f.write("Living in Mymensingh\n")
+#     f.write("I love to code\n")
     
 
-line = ['\nI love python\n', 'I am new to python\n']    
+# line = ['\nI love python\n', 'I am new to python\n']    
 
-with open("name.txt", "a") as f:
-    f.writelines(line)
+# with open("name.txt", "a") as f:
+#     f.writelines(line)
 
-print(content)
+# print(content)
+
+# if os.path.exists("name.txt"):
+#     print("File exists")
+# else:
+#     print("Not extsts")
+
+file_path = pathlib.Path("name.txt")
+
+if file_path:
+    print("yes")
+    print(file_path)
