0b477df761e9112a19977769c365e3f68be3b641
Naymur000 <nayeemur000@gmail.com>
2026-07-01 22:36:19 +0600

    txt2

--- first-code.py
+++ first-code.py
@@ -4,6 +4,8 @@ f_name = "Naymur"
 
 l_name = "Rahman"
 
-txt = "My name is {} {}. I am {} years old.".format(f_name, l_name, age)
+txt = "My name is {f} {l}. I am {a} years old.".format(f=f_name, l=l_name, a=age)
 
-print(txt)
+txt2 = f"My name is {f_name} {l_name}. I am {age} years old."
+
+print(txt2)
